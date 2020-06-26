cf1efd2804d9306b4e4726a480c9960c9ef51836
myuon <ioi.joi.koi.loi@gmail.com>
2020-06-26 23:29:32 +0900

    chore: setup environment

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "standard",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react-hooks"],
+  rules: {
+    "react/prop-types": "off",
+    "no-unused-vars": 0,
+    "@typescript-eslint/no-unused-vars": [2, { args: "none" }],
+    "react/self-closing-comp": 2,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    camelcase: [0],
+  },
+};
