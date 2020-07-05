ae447e5e4ca903593c8ee1d8cce9782032c8ff3f
myuon <ioi.joi.koi.loi@gmail.com>
2020-07-05 20:15:46 +0900

    feat: DropArea

---
+++ src/examples/FileUpload.stories.tsx
@@ -0,0 +1,150 @@
+/** @jsx jsx */
+import React, { useCallback, useRef, useState } from "react";
+import { css, jsx } from "@emotion/core";
+import { Button } from "../Button/Button";
+
+export default {
+  title: "examples",
+};
+
+const DropArea: React.FC<{
+  onSelectFiles?: (files: FileList) => void;
+}> = ({ onSelectFiles }) => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const handleClickDropArea = useCallback(
+    (event: React.MouseEvent<HTMLDivElement>) => {
+      event.preventDefault();
+
+      if (fileInputRef.current) {
+        fileInputRef.current.click();
+      }
+    },
+    []
+  );
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (onSelectFiles && event.currentTarget.files) {
+        onSelectFiles(event.currentTarget.files);
+      }
+    },
+    [onSelectFiles]
+  );
+
+  const dropAreaRef = useRef<HTMLDivElement>(null);
+  const handleDragEnter = useCallback((event: React.DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    event.dataTransfer.dropEffect = "copy";
+
+    if (dropAreaRef.current) {
+      dropAreaRef.current.setAttribute("data-drag-over", "true");
+    }
+  }, []);
+  const handleDragLeave = useCallback((event: React.DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (dropAreaRef.current) {
+      dropAreaRef.current.setAttribute("data-drag-over", "false");
+    }
+  }, []);
+  const handleDrop = useCallback(
+    (event: React.DragEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (onSelectFiles) {
+        onSelectFiles(event.dataTransfer.files);
+      }
+      if (dropAreaRef.current) {
+        dropAreaRef.current.setAttribute("data-drag-over", "false");
+      }
+    },
+    [onSelectFiles]
+  );
+  const preventDefault = useCallback((event: React.DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+  }, []);
+
+  return (
+    <div
+      css={css`
+        input[aria-hidden="true"] {
+          display: none;
+        }
+
+        div[data-drag-over="true"] {
+          border-color: #333;
+        }
+      `}
+    >
+      <input
+        type="file"
+        multiple
+        accept="image/*"
+        aria-hidden={true}
+        ref={fileInputRef}
+        onChange={handleChange}
+      />
+      <div
+        ref={dropAreaRef}
+        role="button"
+        tabIndex={0}
+        css={css`
+          width: 200px;
+          height: 200px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border: 2px dashed #aaa;
+          outline: none;
+
+          color: #333;
+          text-align: center;
+
+          cursor: pointer;
+
+          & * {
+            pointer-events: none;
+          }
+        `}
+        onClick={handleClickDropArea}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+        onDragOver={preventDefault}
+        data-drag-over={false}
+      >
+        <div>
+          <p>Drop file here</p>
+          <p>or</p>
+          <Button tabIndex={-1}>Select Files</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const FileUpload = () => {
+  const [files, setFiles] = useState<string[]>([]);
+  const handleSelectFiles = useCallback((files: FileList) => {
+    setFiles((prev) => {
+      const current = [...prev];
+
+      for (let i = 0, numFiles = files.length; i < numFiles; i++) {
+        current.push(files[i].name);
+      }
+
+      return current;
+    });
+  }, []);
+
+  return (
+    <React.Fragment>
+      <DropArea onSelectFiles={handleSelectFiles} />
+
+      <p>Selected Files: {files.join(",")}</p>
+    </React.Fragment>
+  );
+};
